5b2ff5aff817053c65a10574ff13ac7fe6a8d1e0
Wiebke-Baumann <wiebke.baumann@uni-konstanz.de>
2022-01-31 10:48:25 +0100

    updates and added vignette

--- R/infer_from_text.R
+++ R/infer_from_text.R
@@ -1,29 +1,43 @@
-#' Title infer_from_text
+#' infer_from_text
 #'
-#' @param left the text from the beginning of the sentence to the mention of the
+#' @param left The text from the beginning of the sentence to the mention of the
 #' target (will be empty if the sentence starts with the mention of the target).
 #' Make sure to include a space if there is one between the left phrase and the
-#' target mention, e.g., "I like Peter." -> left="I like ", target="Peter", right=".".""
+#' target mention, e.g., "I like Peter." -> left="I like ", target="Peter",
+#' right=".".""
 #' @param target The mention of the target.
 #' @param right The text from the end of the target to the end of the sentence.
-#' Make sure to include a space if there is one between the target and the next word
-#' of the sentence.
+#' Make sure to include a space if there is one between the target and the next
+#' word of the sentence.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' sentiment <- infer_from_text("I like ", "Peter", " but I don't like Robert.")
+#' print(sentiment[0])
+infer_from_text <- function(left = NULL, target = NULL, right = NULL) {
+  reticulate::use_virtualenv("newssentiment-environment")
+  tsc <- reticulate::import("NewsSentiment.TargetSentimentClassifier")
+  tsc$infer_from_text(left, target, right)
+}
+
+#' Infer by Index
+#'
+#' @param text : The whole text.
+#' @param target_mention_from Beginning Index of Target.
+#' @param target_mention_to End Index of Target.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-create_tsc <- function(){
-  reticulate::py_get_attr(NewsSentiment_p,TargetSentimentClassifier())
+infer_by_index <- function(text_left = NULL,
+                           target_mention = NULL,
+                           text_right = NULL,
+                           text = NULL,
+                           target_mention_from = NULL,
+                           target_mention_to = NULL) {
+  tsc$infer(text, target_mention_from, target_mention_to)
 }
-
-
-#' @export
-infer<- function(left, target, right) {
-  #reticulate::py_load_object("tsc.pickle")
-  tsc <-  pack$tsc
-  tsc$infer_from_text(left, target, right)
-
-}
-
--- R/setup.R
+++ R/setup.R
@@ -1,47 +1,34 @@
 
 #' Package Setup
 #'
-#' @param libname standart
-#' @param pkgname standart
+#' @param libname Generic.
+#' @param pkgname Generic.
 #'
-#' @return tsc
+#' @return
 #' @export
 #'
 #' @examples
 #'
 #'
-#
+#' #
 pack <- new.env(parent = emptyenv())
 NewsSentiment <- NULL
 
 #' @export
-.onload <- function(libname, pkgname){
-  op <- options()
-  op.devtools <- list(
-    devtools.path = "~/R-dev",
-    devtools.install.args = "",
-    devtools.name = "Sentiment4News",
-    devtools.desc.author = " Felix Hamborg <first.last@example.com> [aut, cre]",
-    devtools.desc.license = "What license is it under?",
-    devtools.desc.suggests = NULL,
-    devtools.desc = list()
-  )
-  toset <- !(names(op.devtools) %in% names(op))
-  if(any(toset)) options(op.devtools[toset])
-  invisible()
-  packageStartupMessage("Welcome to NewsSentiment")
+.onload <- function(libname, pkgname) {
+
+  packageStartupMessage("Welcome to NewsSentiment.
+                        The first installation might take a couple of minutes.
+                        But further uses of the package will be much faster.")
 
   version <- "3.7.9"
   reticulate::install_python(version = version)
   reticulate::virtualenv_create("newssentiment-environment", version = version, packages = "NewsSentiment")
   reticulate::use_virtualenv("newssentiment-environment")
-  NewsSentiment_p <<- reticulate::import("NewsSentiment", delay_load = TRUE)
+  # tsc <- reticulate::import("NewsSentiment.TargetSentimentClassifier")
+  NewsSentiment_p <<- reticulate::import("NewsSentiment") # , delay_load = TRUE)
   tsc <<- NewsSentiment_p$TargetSentimentClassifier()
-  reticulate::py_function_wrapper(tsc.infer_from_text(), r_function= "py_infer")
-  pack$tsc<- NewsSentiment_p$TargetSentimentClassifier()
-
-  #reticulate::py_save_object(tsc, "tsc.pickle")
-
+  reticulate::py_function_wrapper(tsc.infer_from_text(), r_function = "py_infer")
+  pack$tsc <- NewsSentiment_p$TargetSentimentClassifier()
 
 }
-
